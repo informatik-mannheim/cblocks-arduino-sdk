30e9f07e998ad4d5ebd632a45bb7df7d04ef705f
unknown <v.weckbach@hs-mannheim.de>
2018-04-24 10:29:04 +0200

    mqtt local

--- examples/room/src/MQTTConf.h
+++ examples/room/src/MQTTConf.h
@@ -1,9 +1,9 @@
 #ifndef MQTT_CONF_H
 #define MQTT_CONF_H
 
-#define MQTT_USERNAME "cdae7db6"
-#define MQTT_PASSWORD "a762129fdb021a34"
-#define MQTT_HOST "broker.shiftr.io"
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
+#define MQTT_HOST "172.16.1.1"
 #define MQTT_PORT 1883
 
 #endif
